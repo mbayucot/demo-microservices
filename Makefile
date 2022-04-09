d4c9102755351553add77f7c45c6864f1d19ee56
Michael Bayucot <mbayucot@gmail.com>
2022-04-09 13:10:04 +0800

    first commit

---
+++ Makefile
@@ -0,0 +1,6 @@
+.PHONY: start-services
+start-services: supergraph-serverless
+
+.PHONY: start-gateway
+start-gateway: supergraph-serverless
+
---
+++ services/subgraphs/users/index.ts
@@ -0,0 +1,23 @@
+import { ApolloServer, gql } from 'apollo-server-lambda';
+import { buildSubgraphSchema } from '@apollo/subgraph';
+import { readFileSync } from 'fs';
+import { resolve } from 'path';
+
+const users = [
+    { email: 'support@apollographql.com', name: "Apollo Studio Support", totalProductsCreated: 4 }
+]
+
+const typeDefs = gql(readFileSync(resolve(__dirname, './users.graphql'), { encoding: 'utf-8' }));
+
+const resolvers = {
+    User: {
+        __resolveReference: (reference) => {
+            return users.find(u => u.email == reference.email);
+        }
+    }
+}
+
+const server = new ApolloServer({
+    schema: buildSubgraphSchema({ typeDefs, resolvers })
+});
+exports.handler = server.createHandler();
